f4c1e97bdf0eb1cc3e162b8aa7fb8a11284b42f4
dahn0209 <dahn0209@gmail.com>
2021-03-17 12:45:05 -0400

    added updateUser component

--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,44 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const userId = req.params.id
+    const user = await User.findByPk(userId)
+
+    //if user doesn't exist
+    if (!user) {
+      console.log('user not found in GET /api/users/id')
+      res.status(404).send('This user does not exist in our database')
+    } else {
+      res.status(200).json(user)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const createUser = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: req.body.password,
+      email: req.body.email
+    })
+
+    res.status(201).json(createUser)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const updateUser = await User.findByPk(req.params.id)
+    res.json(await updateUser.update(req.body))
+  } catch (error) {
+    next(error)
+  }
+})
