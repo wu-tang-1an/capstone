5b616026607b244e7f5027d4d74832df48956403
gene <softhaxorz@gmail.com>
2021-03-17 12:33:22 -0700

    chore: add project model

---
+++ server/db/models/project.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Project = db.define('project', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  status: {
+    type: Sequelize.ENUM('in progress', 'complete'),
+    defaultValue: 'in progress',
+  },
+  description: {
+    type: Sequelize.TEXT,
+    defaultValue: '',
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://unsplash.it/200/200',
+    validate: {
+      isUrl: true,
+    },
+  },
+})
+
+module.exports = Project
