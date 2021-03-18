6a87ed9e88c17d6cf749607a5b4209b0acb927b4
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-17 22:22:12 -0500

    add task modal extensibility logic

--- client/components/TaskCardDropDown.js
+++ client/components/TaskCardDropDown.js
@@ -4,8 +4,8 @@ import TaskModal from './TaskModal'
 
 // fields are actions that user can take from dropdown menu
 const fields = [
-  {id: 1, content: 'Edit'},
-  {id: 2, content: 'Delete'},
+  {id: 1, type: 'Edit'},
+  {id: 2, type: 'Delete'},
   // more fields as necessary
 ]
 
@@ -13,25 +13,45 @@ class TaskCardDropDown extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isActive: false,
+      activeField: '',
     }
+    this.clearActiveField = this.clearActiveField.bind(this)
+  }
+
+  // clear active field closes an open task modal by resetting active field to a falsey value
+  clearActiveField() {
+    this.setState({
+      activeField: '',
+    })
   }
 
   render() {
     const {taskId} = this.props || 0
-    const {isActive} = this.state
+    const {activeField} = this.state
+    const {clearActiveField} = this
+
     return (
       <div className="taskCardDropDownContainer">
-        {fields.map((field) => (
-          <div key={field.id} className="dropDownField">
-            {field.content === 'Edit' ? (
-              <Link to={`/tasks/${taskId}`}>{field.content}</Link>
-            ) : (
-              field.content
-            )}
-            {isActive && <TaskModal type={field.content} />}
-          </div>
-        ))}
+        {/* keep things extensible here by mapping over a fields array and sending the type of field to the TaskModal, which will render with content according to activeField */}
+        {!activeField &&
+          fields.map((field) => (
+            <div
+              key={field.id}
+              className="dropDownField"
+              onClick={() => {
+                this.setState({activeField: field.type})
+              }}
+            >
+              {field.type === 'Edit' ? (
+                <Link to={`/tasks/${taskId}`}>{field.type}</Link>
+              ) : (
+                field.type
+              )}
+            </div>
+          ))}
+        {activeField && (
+          <TaskModal type={activeField} clearActiveField={clearActiveField} />
+        )}
       </div>
     )
   }
--- client/components/TaskModal.js
+++ client/components/TaskModal.js
@@ -1,8 +1,48 @@
 import React from 'react'
 
+// here we can declare different jsx subcomponents
+// and populate our task modal according to the
+// "type" prop passed down by TaskCardDropDown
+
+const deleteTaskJSX = (clearActiveField) => (
+  <div className="deleteTaskModal">
+    <div className="deleteMessage">
+      This is action will remove any cards associated with the column.
+    </div>
+    <button
+      type="button"
+      name="deleteBtn"
+      onClick={() => {
+        // call delete task thunk
+        clearActiveField()
+      }}
+    >
+      Delete Task
+    </button>
+    <button type="button" name="cancelBtn" onClick={clearActiveField}>
+      Cancel
+    </button>
+  </div>
+)
+
+// getModal(type) returns a function that renders JSX according to the type of dropdown container field (edit, delete, etc.)
+// when we invoke this returned function to populate our component's "taskModalContainer", we pass the clearActiveField bound function into it, which allows us to close the modal by resetting the local state of the parent TaskCardDropDown to a falsey value;
+
+const getModal = (type) => {
+  switch (type) {
+    case 'Delete':
+      return deleteTaskJSX
+    default:
+      return () => {}
+  }
+}
+
 const TaskModal = (props) => {
-  const {type} = props
-  return <div className="taskModalContainer">hello world</div>
+  const {type, clearActiveField} = props
+
+  return (
+    <div className="taskModalContainer">{getModal(type)(clearActiveField)}</div>
+  )
 }
 
 export default TaskModal
