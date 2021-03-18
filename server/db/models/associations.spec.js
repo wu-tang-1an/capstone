90e4b833a86b536b0147dff43e0407836a4688aa
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-18 14:40:44 -0500

    add singleUser spec changes and model association tests

--- server/db/models/associations.spec.js
+++ server/db/models/associations.spec.js
@@ -14,10 +14,10 @@ describe('Model Associations', () => {
 
   const task = {
     name: 'create site',
-    createdby: 'albert',
+    createdBy: 'albert',
     description: 'a description of how i will create this site',
     status: 'in-progress',
-    completionDate: '20210315',
+    completionDate: new Date(),
   }
   const user1 = {
     firstName: 'alice',
@@ -61,33 +61,50 @@ describe('Model Associations', () => {
     await newUser1.setOrganizations([newOrganization.id])
     await newUser1.save()
     const foundUser = await User.findByPk(1, {include: Organization})
-    expect(foundUser.organizations[0]).to.include(organization)
+    expect(foundUser.organizations[0]).to.deep.include(organization)
   })
 
-  it('Each User has many Addresses, each Address belongs to many Users', async () => {
-    const newUser = await User.create(user)
-    const newAddress = await Address.create(address)
-    await newUser.setAddresses([newAddress.id])
-    await newUser.save()
-    const foundUser = await User.findByPk(1, {include: Address})
-    expect(foundUser.addresses[0]).to.include(address)
+  it('Each Task belongs to many User, each User belongs to many Task', async () => {
+    const newUser1 = await User.create(user1)
+    const newTask = await Task.create(task)
+    await newUser1.setTasks([newTask.id])
+    await newUser1.save()
+    const foundUser = await User.findByPk(1, {include: Task})
+    expect(foundUser.tasks[0]).to.deep.include(task)
   })
 
-  it('Each Order has many Products, each Product belongs to many Orders', async () => {
-    const newUser = await User.create(user)
-    const newOrder = await Order.create(order)
-    const newProduct = await Product.create(product)
-    await newOrder.setProducts([newProduct.id])
-    await newOrder.save()
-    await newUser.setOrders([newOrder.id])
-    await newUser.save()
-    const foundUser = await User.findByPk(1, {
-      include: {model: Order, include: {model: Product}},
+  it('Each Project belongs to one Organization, each Organization has many Project', async () => {
+    const newOrganization = await Organization.create(organization)
+    const newProject = await Project.create(project)
+    await newOrganization.setProjects([newProject.id])
+    await newOrganization.save()
+    const foundOrganization = await Organization.findByPk(1, {
+      include: [{model: Project}],
     })
+    expect(foundOrganization.projects[0]).to.deep.include(project)
+  })
 
-    expect(foundUser.dataValues).to.have.nested.property(
-      'orders[0].products[0].description',
-      "It's the Pick of Destiny, child!"
-    )
+  it('Each Task belongs to one Column, each Column has many Task', async () => {
+    const newTask = await Task.create(task)
+    const newColumn = await Column.create(column1)
+    await newColumn.setTasks([newTask.id])
+    await newColumn.save()
+    const foundColumn = await Column.findByPk(1, {
+      include: [{model: Task}],
+    })
+    expect(foundColumn.tasks[0]).to.deep.include(task)
+  })
+
+  it('Each Column belongs to one Project, each Project has many Columns', async () => {
+    const newProject = await Project.create(project)
+    const newColumn1 = await Column.create(column1)
+    const newColumn2 = await Column.create(column2)
+    await newProject.setColumns([newColumn1.id, newColumn2.id])
+    await newProject.save()
+    const foundProject = await Project.findByPk(1, {
+      include: [{model: Column}],
+    })
+    expect(foundProject.columns[0]).to.deep.include(column1)
+    expect(foundProject.columns[1]).to.deep.include(column2)
   })
 })
