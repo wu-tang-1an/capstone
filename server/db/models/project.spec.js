98471e95c43ffe962f05af2bbb36a51a90f25494
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-18 15:21:51 -0500

    add project model specs

---
+++ server/db/models/project.spec.js
@@ -0,0 +1,46 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const Project = db.model('project')
+
+describe('Project Model', () => {
+  beforeEach(() => db.sync({force: true}))
+  afterEach(() => db.sync({force: true}))
+
+  it('has fields name, status, description, imageUrl', async () => {
+    try {
+      const project = await Project.create({
+        name: 'super cool project',
+        status: 'in progress',
+        description: 'this project is super cool!',
+        imageUrl: 'http://www.image.com',
+      })
+      const foundProject = await Project.findByPk(1)
+      expect(foundProject.dataValues).to.deep.include({
+        name: 'super cool project',
+        status: 'in progress',
+        description: 'this project is super cool!',
+        imageUrl: 'http://www.image.com',
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  })
+
+  it('uses a default image if none is provided', async () => {
+    try {
+      const project = await Project.create({
+        name: 'super cool project',
+        status: 'in progress',
+        description: 'this project is super cool!',
+      })
+      const foundProject = await Project.findByPk(1)
+      expect(foundProject.dataValues).to.deep.include({
+        imageUrl: 'https://unsplash.it/200/200',
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  })
+})
