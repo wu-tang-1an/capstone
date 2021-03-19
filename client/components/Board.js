abb8d44332eefcfada160bcc7c778cb3e60e7d81
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-19 15:46:06 -0500

    convert task card to fn component

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
 import {connect} from 'react-redux'
 import Column from './Column'
 import AddButton from './AddButton'
@@ -8,18 +10,19 @@ class Board extends React.Component {
   // const {columns} = props
 
   // fakeDb, remove when connected to real db
-
   render() {
     const {columns} = this.props
     return (
-      <div className="Board">
-        <div className={styles.boardContainer}>
-          {columns.map((column) => (
-            <Column key={column.id} column={column} />
-          ))}
-          <AddButton column />
+      <DndProvider backend={HTML5Backend}>
+        <div className="Board">
+          <div className={styles.boardContainer}>
+            {columns.map((column) => (
+              <Column key={column.id} column={column} />
+            ))}
+            <AddButton column />
+          </div>
         </div>
-      </div>
+      </DndProvider>
     )
   }
 }
--- client/components/TaskCard.js
+++ client/components/TaskCard.js
@@ -1,56 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import TaskCardDropDown from './TaskCardDropDown'
+import {ItemTypes} from './DragConstants'
 
 import styles from './TaskCard.css'
-class TaskCard extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      // if inactive, onClick dotMenu shows a dropdown
-      isActive: false,
-    }
-  }
+const TaskCard = (props) => {
+  const [isActive, setActive] = useState(false)
 
-  componentDidMount() {
-    // mount task here
-  }
+  const {id, name, createdBy, createdAt, description, status, user} = props.task
 
-  render() {
-    const {
-      id,
-      name,
-      createdBy,
-      createdAt,
-      description,
-      status,
-      user,
-    } = this.props.task
-    const {isActive} = this.state
-    return (
-      <div>
-        {isActive && <TaskCardDropDown taskId={id} />}
-        <div className={styles.taskCardContainer}>
-          <div className="material-icons">error_outline</div>
-          <div className={styles.titleAndCreator}>
-            <div className={styles.title}>{name}</div>
-            <div
-              className={styles.idAndCreatedBy}
-            >{`# opened by ${user.name}`}</div>
-          </div>
-          <div className={styles.dotMenuAndAvatar}>
-            <span
-              className="material-icons"
-              onClick={() => this.setState({isActive: !isActive})}
-            >
-              more_horiz
-            </span>
-            <img src={user.imageUrl} />
-          </div>
+  return (
+    <div draggable={true}>
+      {isActive && <TaskCardDropDown taskId={id} />}
+      <div className={styles.taskCardContainer}>
+        <div className="material-icons">error_outline</div>
+        <div className={styles.titleAndCreator}>
+          <div className={styles.title}>{name}</div>
+          <div
+            className={styles.idAndCreatedBy}
+          >{`# opened by ${user.name}`}</div>
+        </div>
+        <div className={styles.dotMenuAndAvatar}>
+          <span className="material-icons" onClick={() => setActive(!isActive)}>
+            more_horiz
+          </span>
+          <img src={user.imageUrl} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default TaskCard
