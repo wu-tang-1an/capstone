70ea8a94a3ca862fd693f6339edb57c905edf83a
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-22 16:06:42 -0500

    refactor: single task expanded

--- client/components/Modal.js
+++ client/components/Modal.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Modal.css'
 
-const Modal = (props) => (
-  <div>
-    <div className={styles.modalBackdrop}></div>
-    <div className={styles.transparentBlockContainer}>
-      <div className={styles.centeredModalBlock}>
-        <div className={styles.modalTitleAndCloseBtn}>
-          <div className={styles.modalTitle}>Delete Task</div>
-          <div className={styles.closeModalBtn}>
-            <span className="material-icons" /* onClick={handleCloseModal} */>
-              close
-            </span>
+const Modal = (props) => {
+  return (
+    <div>
+      <div className={styles.modalBackdrop}></div>
+      <div className={styles.transparentBlockContainer}>
+        <div className={styles.centeredModalBlock}>
+          <div className={styles.modalTitleAndCloseBtn}>
+            <div className={styles.modalTitle}>Delete Task</div>
+            <div className={styles.closeModalBtn}>
+              <span className="material-icons" /* onClick={handleCloseModal} */>
+                close
+              </span>
+            </div>
           </div>
+          <div className={styles.modalContent}>{props.children}</div>
         </div>
-        <div className={styles.modalContent}>{props.children}</div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Modal
--- client/components/SingleTaskExpanded.js
+++ client/components/SingleTaskExpanded.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState, useContext} from 'react'
+import {ProjectContext} from './ProjectProvider'
 import marked from 'marked'
 import Comment from './Comment'
-import {connect} from 'react-redux'
-import {fetchSingleTask} from '../store/singleTask'
 
 const fakeCommentsDB = [
   {
@@ -30,138 +28,91 @@ const fakeCommentsDB = [
 ]
 
 import styles from './SingleTaskExpanded.css'
-class SingleTaskExpanded extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: 'create homepage',
-      issueType: 'todo',
-      description: '## markdown is here\n~lets see if it works~',
-      collaborators: [],
-      activeMarkdownEditor: false,
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
 
-  componentDidMount() {
-    // mount task data here
-    // this.props.getSingleTask(this.props.match.params.taskId)
-  }
+const SingleTaskExpanded = ({taskId}) => {
+  const {tasks, setTasks} = useContext(ProjectContext)
 
-  /* componentDidUpdate(prevProps) {
-    const prevId = prevProps.match.params.taskId
-    const currId = this.props.match.params.taskId
-    if (!prevId && currId) {
-      this.props.getSingleTask(currId)
-    }
-  } */
+  const thisTask = tasks.find((task) => task.id === taskId)
 
-  handleChange(evt) {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-    })
-  }
+  const [name, setName] = useState(thisTask.name)
+  const [issueType, setIssueType] = useState(thisTask.issueType)
+  const [description, setDescription] = useState(thisTask.description)
+  const [activeMarkdownEditor, setActiveMarkdownEditor] = useState(false)
 
-  handleUpdateTask() {
-    // call thunk to update task here
-  }
+  const comments = fakeCommentsDB
 
-  handleDeleteTask() {
-    // call thunk to delete task here
-  }
-
-  render() {
-    const {name, issueType} = this.state
-    const {handleCloseModal} = this.props
-    // const {comments} = this.props || []
-
-    // fakeDb: remove after connecting to real db
-    const comments = fakeCommentsDB
-
-    const {handleChange, handleUpdateTask, handleDeleteTask} = this
-    return (
-      <div>
-        <div className={styles.singleTaskContainer}>
-          <div className={styles.leftPanel}>
-            <div className={styles.nameAndIssue}>
-              <span className={styles.taskName}>Task: {name}</span>
-              <span className={styles.issueType}>Issue type: {issueType}</span>
-            </div>
-            <div>
-              <div className={styles.containerLabel}>Description:</div>
-              {this.state.activeMarkdownEditor ? (
-                <textarea
-                  className={styles.descriptionMarkdown}
-                  ref={(input) => input && input.focus()}
-                  onBlur={() =>
-                    this.setState({
-                      activeMarkdownEditor: false,
-                    })
-                  }
-                  name="description"
-                  value={this.state.description || ''}
-                  onChange={handleChange}
-                ></textarea>
-              ) : (
-                <div
-                  className={styles.descriptionMarkdown}
-                  onClick={() =>
-                    this.setState({
-                      activeMarkdownEditor: true,
-                    })
-                  }
-                  dangerouslySetInnerHTML={{
-                    __html: marked(this.state.description),
-                  }}
-                ></div>
-              )}
-            </div>
-            <div>
-              <div className={styles.containerLabel}>Comments:</div>
-              <div className={styles.commentsContainer}>
-                {comments.map((comment) => (
-                  <Comment key={comment.id} comment={comment} />
-                ))}
-              </div>
-            </div>
-            <div className={styles.updateAndDeleteBtns}>
-              <button type="button" name="update" onClick={handleUpdateTask}>
-                Update Task
-              </button>
-              <button type="button" name="delete" onClick={handleDeleteTask}>
-                Delete Task
-              </button>
-            </div>
-            <div className={styles.backToProjectView}>
-              <a
-                href="#"
-                className={styles.spanContainer}
-                onClick={handleCloseModal}
-              >
-                <span className="material-icons">keyboard_arrow_left</span>
-                <span>Back to project</span>
-              </a>
+  return (
+    <div>
+      <div className={styles.singleTaskContainer}>
+        <div className={styles.leftPanel}>
+          <div className={styles.nameAndIssue}>
+            <span className={styles.taskName}>Task: {name}</span>
+            <span className={styles.issueType}>Issue type: {issueType}</span>
+          </div>
+          <div>
+            <div className={styles.containerLabel}>Description:</div>
+            {activeMarkdownEditor ? (
+              <textarea
+                className={styles.descriptionMarkdown}
+                ref={(input) => input && input.focus()}
+                onBlur={() => setActiveMarkdownEditor(false)}
+                name="description"
+                value={description || ''}
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
+            ) : (
+              <div
+                className={styles.descriptionMarkdown}
+                onClick={() => setActiveMarkdownEditor(true)}
+                dangerouslySetInnerHTML={{
+                  __html: marked(description),
+                }}
+              ></div>
+            )}
+          </div>
+          <div>
+            <div className={styles.containerLabel}>Comments:</div>
+            <div className={styles.commentsContainer}>
+              {comments.map((comment) => (
+                <Comment key={comment.id} comment={comment} />
+              ))}
             </div>
           </div>
-          <div className={styles.rightPanel}>
-            <div className={styles.label}>label</div>
-            <div className={styles.members}>members</div>
-            <div className={styles.attachFile}>attachFile</div>
-            <div className={styles.projectDate}>projectDate</div>
-            <div className={styles.taskChecklist}>taskChecklist</div>
+          <div className={styles.updateAndDeleteBtns}>
+            <button
+              type="button"
+              name="update" /* onClick={handleUpdateTask} */
+            >
+              Update Task
+            </button>
+            <button
+              type="button"
+              name="delete" /* onClick={handleDeleteTask} */
+            >
+              Delete Task
+            </button>
+          </div>
+          <div className={styles.backToProjectView}>
+            <a
+              href="#"
+              className={styles.spanContainer}
+              /* onClick={handleCloseModal} */
+            >
+              <span className="material-icons">keyboard_arrow_left</span>
+              <span>Back to project</span>
+            </a>
           </div>
         </div>
+        <div className={styles.rightPanel}>
+          <div className={styles.label}>label</div>
+          <div className={styles.members}>members</div>
+          <div className={styles.attachFile}>attachFile</div>
+          <div className={styles.projectDate}>projectDate</div>
+          <div className={styles.taskChecklist}>taskChecklist</div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-const mapState = (state) => ({
-  singleTask: state.singleTask,
-})
-
-const mapDispatch = (dispatch) => ({
-  getSingleTask: (taskId) => dispatch(fetchSingleTask(taskId)),
-})
-
-export default connect(mapState, mapDispatch)(SingleTaskExpanded)
+export default SingleTaskExpanded
