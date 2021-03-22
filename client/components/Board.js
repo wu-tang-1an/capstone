f82957c5c4b70e26da03b2bc1f3ddd3cb5f713ad
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-22 13:35:33 -0500

    add top level provider to project view

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,30 +1,48 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, {useContext} from 'react'
 import Column from './Column'
 import AddButton from './AddButton'
 import styles from './Board.css'
+import {ProjectContext} from './ProjectProvider'
 
-class Board extends React.Component {
+const fakeDb = [
+  {
+    id: 1,
+    name: 'Todo',
+  },
+  {
+    id: 2,
+    name: 'In-progress',
+  },
+  {
+    id: 3,
+    name: 'Review',
+  },
+  {
+    id: 4,
+    name: 'Done',
+  },
+]
+
+const Board = () => {
   // const {columns} = props
 
   // fakeDb, remove when connected to real db
-  render() {
-    const {columns} = this.props
-    return (
-      <div className="Board">
-        <div className={styles.boardContainer}>
-          {columns.map((column) => (
-            <Column key={column.id} column={column} />
-          ))}
-          <AddButton column />
-        </div>
+  const columns = fakeDb
+
+  const {tasks, setTasks, users, setUsers} = useContext(ProjectContext)
+  console.log('users in board component, ', users)
+
+  console.log('tasks in Board component, ', tasks)
+  return (
+    <div className="Board">
+      <div className={styles.boardContainer}>
+        {columns.map((column) => (
+          <Column key={column.id} column={column} />
+        ))}
+        <AddButton column />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => ({
-  columns: state.columns,
-})
-
-export default connect(mapStateToProps)(Board)
+export default Board
