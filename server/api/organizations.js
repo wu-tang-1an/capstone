3e9311dad1bdee6003133db8afe72cfa879cd10a
gene <softhaxorz@gmail.com>
2021-03-22 10:02:58 -0700

    chore: add more missing routes to organization api

--- server/api/organizations.js
+++ server/api/organizations.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User, UserOrganization, Organization} = require('../db/models')
+const {Organization, User} = require('../db/models')
 const {checkUser, checkAdmin} = require('./gatekeeper')
 module.exports = router
 
@@ -8,6 +8,21 @@ router.get('/', checkAdmin, async (req, res, next) => {
   try {
     const orgs = await Organization.findAll()
     res.json(orgs)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET single organizations route '/api/organizations/:orgId' (AUTH USER ONLY)
+router.get('/:orgId', checkUser, async (req, res, next) => {
+  try {
+    const {orgId} = req.params
+
+    if (isNaN(orgId)) res.status(400).send(orgId + ' is not a number!')
+    else {
+      const org = await Organization.findByPk(orgId)
+      res.json(org)
+    }
   } catch (error) {
     next(error)
   }
@@ -28,6 +43,51 @@ router.get('/:orgId/users', checkUser, async (req, res, next) => {
       const users = await org.getUsers()
       res.json(users)
     }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// POST create new organization route '/api/organizations/' (AUTH USER ONLY)
+router.post('/', checkUser, async (req, res, next) => {
+  try {
+    const data = req.body
+    const {dataValues} = await Organization.create(data)
+
+    res.json(dataValues)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// PUT edit organization route '/api/organizations/:orgId' (AUTH USER ONLY)
+router.put('/:orgId', checkUser, async (req, res, next) => {
+  try {
+    const data = req.body
+    const {orgId} = req.params
+
+    const [updatedRows, updatedOrg] = await Organization.update(data, {
+      plain: true,
+      returning: true,
+      where: {id: orgId},
+    }) // returns array: [updatedRows, {updatedProject}]
+
+    res.json(updatedOrg)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// DELETE organization route '/api/organizations/:orgId' (AUTH USER ONLY)
+router.delete('/:orgId', checkUser, async (req, res, next) => {
+  try {
+    const {orgId} = req.params
+
+    if (isNaN(orgId)) res.status(400).send(orgId + ' is not a number!')
+
+    await Organization.destroy({where: {id: orgId}})
+
+    res.status(204).end()
   } catch (error) {
     next(error)
   }
