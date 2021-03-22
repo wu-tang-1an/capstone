4a5067da519e72aadd1bbd1c1677edbcbb8c4fc5
gene <softhaxorz@gmail.com>
2021-03-22 07:36:20 -0700

    maint: clean organizations api routes

--- server/api/organizations.js
+++ server/api/organizations.js
@@ -1,33 +1,34 @@
 const router = require('express').Router()
 const {User, UserOrganization, Organization} = require('../db/models')
+const {checkUser, checkAdmin} = require('./gatekeeper')
 module.exports = router
 
-// get all the orfs in the app
-router.get('/', async (req, res, next) => {
+// GET all organizations route '/api/organizations' (ADMIN ONLY)
+router.get('/', checkAdmin, async (req, res, next) => {
   try {
     const orgs = await Organization.findAll()
     res.json(orgs)
-  } catch (err) {
-    next(err)
+  } catch (error) {
+    next(error)
   }
 })
 
-// get the users in the org
-router.get('/:orgId/users', async (req, res, next) => {
-  const {orgId} = req.params
-
+// GET single organization's users route '/api/organizations/:orgId/users' (AUTH USER ONLY)
+router.get('/:orgId/users', checkUser, async (req, res, next) => {
   try {
-    //test route
-    const OrgFound = await Organization.findOne({
-      where: {
-        id: orgId,
-      },
-      include: User,
-    })
+    const {orgId} = req.params
 
-    res.json(OrgFound)
-  } catch (e) {
-    console.log(e)
-    next(e)
+    if (isNaN(orgId)) res.status(400).send(orgId + ' is not a number!')
+    else {
+      const org = await Organization.findByPk(orgId)
+
+      // if organization doesn't exist
+      if (!org) res.status(404).send('Organization not found in database!')
+
+      const users = await org.getUsers()
+      res.json(users)
+    }
+  } catch (error) {
+    next(error)
   }
 })
