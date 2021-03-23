eabd89bf30a712289f0a40057072b64b444803d5
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-23 08:31:32 -0500

    fix: add admin status to users seed

--- script/seedFiles/users.js
+++ script/seedFiles/users.js
@@ -4,23 +4,27 @@ module.exports = [
     lastName: 'Shapiro',
     email: 'daniel@mail.com',
     password: '123',
+    status: 'admin',
   },
   {
     firstName: 'David',
     lastName: 'Ahn',
     email: 'dahn@mail.com',
     password: '123',
+    status: 'admin',
   },
   {
     firstName: 'Gene',
     lastName: 'Kaufman',
     email: 'gkaufman@mail.com',
     password: '123',
+    status: 'admin',
   },
   {
     firstName: 'Johnny',
     lastName: 'Vazquez',
     email: 'johnny@email.com',
     password: '123',
+    status: 'admin',
   },
 ]
