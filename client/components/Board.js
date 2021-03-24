b6a78fc19ca70548d53a2588b6e8b71934356f7d
gene <softhaxorz@gmail.com>
2021-03-24 13:25:02 -0700

    feat: drop logic wip

--- client/components/Board.js
+++ client/components/Board.js
@@ -6,13 +6,55 @@ import Column from './Column'
 import AddColumnDropDown from './AddColumnDropDown'
 import styles from './css/Board.css'
 
-const onDragEnd = (result) => {
-  // TODO
-}
-
 const Board = () => {
   // get columns from ProjectContext
-  const {project, columns} = useContext(ProjectContext)
+  const {project, columns, setColumns} = useContext(ProjectContext)
+
+  // drop logic
+  const onDragEnd = (result) => {
+    const {draggableId, source, destination} = result
+
+    console.log('draggableId: ', draggableId)
+    console.log('source: ', source)
+    console.log('destination: ', destination)
+
+    if (!destination) return
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return
+
+    const column = columns.find((col) => col.id === +source.droppableId)
+
+    console.log('columns: ', columns)
+    console.log('column: ', column)
+
+    const newTasks = Array.from(column.tasks)
+    const moveTask = newTasks.find((task) => task.id === +draggableId)
+
+    console.log('oldTasks: ', column.tasks)
+    console.log('moveTask: ', moveTask)
+
+    newTasks.splice(source.index, 1)
+    newTasks.splice(destination.index, 0, moveTask)
+
+    console.log('newTasks: ', newTasks)
+
+    const newColumn = {
+      ...column,
+      tasks: newTasks,
+    }
+
+    console.log('newColumn: ', newColumn)
+
+    const newColumns = [...columns]
+
+    console.log('newColumns: ', newColumns)
+
+    setColumns([newColumn])
+  }
 
   return (
     <div className="Board">
