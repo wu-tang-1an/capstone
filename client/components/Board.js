7a59c8c153d59bae1f1020b69a1d4d4549a314d2
dahn0209 <dahn0209@gmail.com>
2021-03-24 12:34:50 -0400

    added DragDropContext in Board.js

--- client/components/Board.js
+++ client/components/Board.js
@@ -4,23 +4,26 @@ import ColumnProvider from '../context/columnContext'
 import Column from './Column'
 import AddColumnDropDown from './AddColumnDropDown'
 import styles from './css/Board.css'
+import {DragDropContext} from 'react-beautiful-dnd'
 
 const Board = () => {
   // get columns from ProjectContext
   const {project, columns} = useContext(ProjectContext)
 
   return (
-    <div className="Board">
-      <h2>Project: {project.name}</h2>
-      <div className={styles.boardContainer}>
-        {columns.map((column) => (
-          <ColumnProvider key={column.id} columnId={column.id}>
-            <Column />
-          </ColumnProvider>
-        ))}
-        <AddColumnDropDown />
+    <DragDropContext>
+      <div className="Board">
+        <h2>Project: {project.name}</h2>
+        <div className={styles.boardContainer}>
+          {columns.map((column) => (
+            <ColumnProvider key={column.id} columnId={column.id}>
+              <Column />
+            </ColumnProvider>
+          ))}
+          <AddColumnDropDown />
+        </div>
       </div>
-    </div>
+    </DragDropContext>
   )
 }
 
