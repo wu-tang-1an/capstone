09a0e7cc0e45526aba8732ae061769d5cb57083b
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-24 14:21:37 -0500

    add onChange fn to track column name input, add project context, remove auth context and column context

--- client/components/AddColumnDialog.js
+++ client/components/AddColumnDialog.js
@@ -1,20 +1,45 @@
-import React, {useContext} from 'react'
+import React, {useState, useContext} from 'react'
 import styles from './css/AddColumnDialog.css'
 import {ProjectContext} from '../context/projectContext'
-import {ColumnContext} from '../context/columnContext'
+import {addColumnDB} from '../context/axiosService'
 
 const AddColumnDialog = ({cancel}) => {
-  // grab project from project context
-  const {project, setProject} = useContext(ProjectContext)
+  // grab project and its columns and setters from project context
+  const {project, setProject, columns, setColumns} = useContext(ProjectContext)
 
-  // grab columns from column context
-  const {columns, setColumns} = useContext(ColumnContext)
+  // initialize local state for new column name
+  const [name, setName] = useState('')
+
+  // add column method updates db and closes the dialog
+  const addColumn = async (e) => {
+    e.preventDefault()
+
+    const newColumn = {
+      name: name,
+    }
+
+    try {
+      const createdColumn = await addColumnDB(newColumn)
+      project.addColumn(createdColumn)
+      setColumns([...columns, createdColumn])
+      cancel()
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
     <div className={styles.addColumnDropDownContainer}>
-      <textarea className={styles.description}></textarea>
+      <textarea
+        className={styles.description}
+        onChange={(e) => setName(e.target.value)}
+      ></textarea>
       <div className={styles.btnContainer}>
-        <button type="button" className={styles.addBtn}>
+        <button
+          type="button"
+          className={styles.addBtn}
+          onClick={(e) => addColumn(e, name)}
+        >
           Add column
         </button>
         <button type="button" className={styles.cancelBtn} onClick={cancel}>
