567c6be4078a99fada6d6ad87a9a6346b6a567c1
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-26 13:31:24 -0500

    revise: board layout

--- client/components/ProjectView.js
+++ client/components/ProjectView.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {withRouter} from 'react-router'
 import ProjectProvider from '../context/projectContext'
 import Board from './Board'
+import styles from './css/ProjectView.css'
 
 const ProjectView = ({match}) => {
   const projectId = +match.params.projectId
 
   return (
-    <div className="topLevelViewContainer">
+    <div className={styles.projectContainer}>
       <ProjectProvider projectId={projectId}>
         <Board />
       </ProjectProvider>
