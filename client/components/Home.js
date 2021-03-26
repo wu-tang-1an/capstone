0c16d39ec7c6579abc6944b449aa520185ccf3a1
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-26 06:41:50 -0500

    fix: user.firstName in Home

--- client/components/Home.js
+++ client/components/Home.js
@@ -5,10 +5,12 @@ import styles from './css/Home.css'
 const Home = () => {
   const {user} = useContext(AuthContext)
 
+  console.log('user is: ', user)
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.welcome}>
-        <span>Welcome, {user.name}!</span>
+        <span>Welcome, {user.firstName}!</span>
       </div>
       <div className={styles.myFeed}></div>
       {/* chat component here */}
