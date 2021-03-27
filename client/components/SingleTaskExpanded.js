278bab76268fc823899f8f1409a5849e9118d203
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-27 05:43:04 -0500

    refactor: single task layout, add name field to Task model, update seed tasks to include name and isActiveBadge status

--- client/components/SingleTaskExpanded.js
+++ client/components/SingleTaskExpanded.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react'
 import marked from 'marked'
 import Comment from './Comment'
+import ImportantBadge from './ImportantBadge'
 import styles from './css/SingleTaskExpanded.css'
 
 const SingleTaskExpanded = ({task, closeModal}) => {
-  const {description, createdBy, isActiveBadge, updatedAt} = task || {}
+  const {id, description, createdBy, isActiveBadge, updatedAt} = task || {}
 
   // destructure comments separately to type check
   const comments = task && task.comments ? task.comments : []
@@ -17,78 +18,84 @@ const SingleTaskExpanded = ({task, closeModal}) => {
   return (
     <div>
       <div className={styles.singleTaskContainer}>
-        <div className={styles.leftPanel}>
-          <div className={styles.nameAndCreator}>
-            <span className={styles.taskName}>Task: {description}</span>
-            <span className={styles.creator}>Created by: {creator}</span>
+        <div className={styles.nameAndCreator}>
+          <div className={styles.badgeNameId}>
+            <span className={styles.inlineBadge}>
+              <ImportantBadge isActiveBadge={isActiveBadge} />
+            </span>
+            <span className={styles.taskName}>{description}</span>
+            <span className={styles.taskId}>{`# ${id}`}</span>
           </div>
-          <div>
-            <div className={styles.containerLabel}>Description:</div>
-
-            {/* when markdown editor has focus, it is a textarea */}
-            {activeMarkdownEditor && (
-              <textarea
-                className={styles.descriptionMarkdown}
-                ref={(input) => input && input.focus()}
-                onBlur={() => setActiveMarkdownEditor(false)}
-                name="description"
-                value={taskDescription || ''}
-                onChange={(e) => setDescription(e.target.value)}
-              ></textarea>
-            )}
-
-            {/* when markdown editor does not have focus it is a div that renders its innerHTML as markdown */}
-            {!activeMarkdownEditor && (
-              <div
-                className={styles.descriptionMarkdown}
-                onClick={() => setActiveMarkdownEditor(true)}
-                dangerouslySetInnerHTML={{
-                  __html: marked(taskDescription),
+          <span className={styles.creator}>{creator}</span>
+        </div>
+        <div className={styles.panels}>
+          <div className={styles.leftPanel}>
+            <div>
+              <div className={styles.containerLabel}>Description:</div>
+              {/* when markdown editor has focus, it is a textarea */}
+              {activeMarkdownEditor && (
+                <textarea
+                  className={styles.descriptionMarkdown}
+                  ref={(input) => input && input.focus()}
+                  onBlur={() => setActiveMarkdownEditor(false)}
+                  name="description"
+                  value={taskDescription || ''}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              )}
+              {/* when markdown editor does not have focus it is a div that renders its innerHTML as markdown */}
+              {!activeMarkdownEditor && (
+                <div
+                  className={styles.descriptionMarkdown}
+                  onClick={() => setActiveMarkdownEditor(true)}
+                  dangerouslySetInnerHTML={{
+                    __html: marked(taskDescription),
+                  }}
+                ></div>
+              )}
+            </div>
+            <div>
+              <div className={styles.containerLabel}>Comments:</div>
+              <div className={styles.commentsContainer}>
+                {comments.map((comment) => (
+                  <Comment key={comment.id} comment={comment} />
+                ))}
+              </div>
+            </div>
+            <div className={styles.updateAndDeleteBtns}>
+              <button
+                type="button"
+                name="update" /* onClick={handleUpdateTask} */
+              >
+                Update Task
+              </button>
+              <button
+                type="button"
+                name="delete" /* onClick={handleDeleteTask} */
+              >
+                Delete Task
+              </button>
+            </div>
+            <div className={styles.backToProjectView}>
+              <a
+                href="#"
+                className={styles.spanContainer}
+                onClick={() => {
+                  closeModal()
                 }}
-              ></div>
-            )}
-          </div>
-          <div>
-            <div className={styles.containerLabel}>Comments:</div>
-            <div className={styles.commentsContainer}>
-              {comments.map((comment) => (
-                <Comment key={comment.id} comment={comment} />
-              ))}
+              >
+                <span className="material-icons">keyboard_arrow_left</span>
+                <span>Back to project</span>
+              </a>
             </div>
           </div>
-          <div className={styles.updateAndDeleteBtns}>
-            <button
-              type="button"
-              name="update" /* onClick={handleUpdateTask} */
-            >
-              Update Task
-            </button>
-            <button
-              type="button"
-              name="delete" /* onClick={handleDeleteTask} */
-            >
-              Delete Task
-            </button>
+          <div className={styles.rightPanel}>
+            <div className={styles.label}>label</div>
+            <div className={styles.members}>members</div>
+            <div className={styles.attachFile}>attachFile</div>
+            <div className={styles.projectDate}>projectDate</div>
+            <div className={styles.taskChecklist}>taskChecklist</div>
           </div>
-          <div className={styles.backToProjectView}>
-            <a
-              href="#"
-              className={styles.spanContainer}
-              onClick={() => {
-                closeModal()
-              }}
-            >
-              <span className="material-icons">keyboard_arrow_left</span>
-              <span>Back to project</span>
-            </a>
-          </div>
-        </div>
-        <div className={styles.rightPanel}>
-          <div className={styles.label}>label</div>
-          <div className={styles.members}>members</div>
-          <div className={styles.attachFile}>attachFile</div>
-          <div className={styles.projectDate}>projectDate</div>
-          <div className={styles.taskChecklist}>taskChecklist</div>
         </div>
       </div>
     </div>
--- server/db/models/task.js
+++ server/db/models/task.js
@@ -3,6 +3,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Task = db.define('task', {
+  name: {
+    type: Sequelize.STRING,
+  },
   description: {
     type: Sequelize.TEXT,
   },
