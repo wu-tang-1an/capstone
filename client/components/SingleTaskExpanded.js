7593b930d32faebeb7af7e3f192b093ef8b903c6
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-27 06:28:12 -0500

    fix: single task markdown textarea layout

--- client/components/SingleTaskExpanded.js
+++ client/components/SingleTaskExpanded.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import marked from 'marked'
+import moment from 'moment'
 import Comment from './Comment'
 import ImportantBadge from './ImportantBadge'
 import styles from './css/SingleTaskExpanded.css'
@@ -28,75 +29,86 @@ const SingleTaskExpanded = ({task, closeModal}) => {
             <span className={styles.taskName}>{taskName}</span>
             <span className={styles.taskId}>{`# ${id}`}</span>
           </div>
-          <span className={styles.creator}>{creator}</span>
+          <span className={styles.creator}>{`Opened by ${creator}`}</span>
+          <span className={styles.lastEdited}>{`Last edit: ${moment(
+            updatedAt
+          ).fromNow()}`}</span>
         </div>
-        <div className={styles.panels}>
-          <div className={styles.leftPanel}>
-            <div>
-              <div className={styles.containerLabel}>Description:</div>
-              {/* when markdown editor has focus, it is a textarea */}
-              {activeMarkdownEditor && (
-                <textarea
-                  className={styles.descriptionMarkdown}
-                  ref={(input) => input && input.focus()}
-                  onBlur={() => setActiveMarkdownEditor(false)}
-                  name="description"
-                  value={taskDescription || ''}
-                  onChange={(e) => setDescription(e.target.value)}
-                ></textarea>
-              )}
-              {/* when markdown editor does not have focus it is a div that renders its innerHTML as markdown */}
-              {!activeMarkdownEditor && (
-                <div
-                  className={styles.descriptionMarkdown}
-                  onClick={() => setActiveMarkdownEditor(true)}
-                  dangerouslySetInnerHTML={{
-                    __html: marked(taskDescription),
-                  }}
-                ></div>
-              )}
+
+        <div className={styles.mainPanel}>
+          <div className={styles.descriptionContainer}>
+            <div className={styles.containerLabel}>
+              <span>Task description</span>
+              <span className={styles.smol}>
+                <i className="material-icons">mode</i>click to edit
+              </span>
             </div>
-            <div>
-              <div className={styles.containerLabel}>Comments:</div>
-              <div className={styles.commentsContainer}>
-                {comments.map((comment) => (
-                  <Comment key={comment.id} comment={comment} />
-                ))}
-              </div>
-            </div>
-            <div className={styles.updateAndDeleteBtns}>
-              <button
-                type="button"
-                name="update" /* onClick={handleUpdateTask} */
-              >
-                Update Task
-              </button>
-              <button
-                type="button"
-                name="delete" /* onClick={handleDeleteTask} */
-              >
-                Delete Task
-              </button>
-            </div>
-            <div className={styles.backToProjectView}>
-              <a
-                href="#"
-                className={styles.spanContainer}
-                onClick={() => {
-                  closeModal()
+            {/* when markdown editor has focus, it is a textarea */}
+            {activeMarkdownEditor && (
+              <textarea
+                className={styles.descriptionMarkdown}
+                ref={(input) => input && input.focus()}
+                onBlur={() => setActiveMarkdownEditor(false)}
+                name="description"
+                value={taskDescription || ''}
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
+            )}
+            {/* when markdown editor does not have focus it is a div that renders its innerHTML as markdown */}
+            {!activeMarkdownEditor && (
+              <div
+                className={styles.descriptionMarkdown}
+                onClick={() => setActiveMarkdownEditor(true)}
+                dangerouslySetInnerHTML={{
+                  __html: marked(taskDescription),
                 }}
+              ></div>
+            )}
+            <span className={styles.markdownLink}>
+              (new to markdown?
+              <a
+                href="https://www.markdownguide.org/getting-started/"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <span className="material-icons">keyboard_arrow_left</span>
-                <span>Back to project</span>
+                {' click here for an overview'}
               </a>
+              )
+            </span>
+          </div>
+          <div>
+            <div className={styles.containerLabel}>Comments:</div>
+            <div className={styles.commentsContainer}>
+              {comments.map((comment) => (
+                <Comment key={comment.id} comment={comment} />
+              ))}
             </div>
           </div>
-          <div className={styles.rightPanel}>
-            <div className={styles.label}>label</div>
-            <div className={styles.members}>members</div>
-            <div className={styles.attachFile}>attachFile</div>
-            <div className={styles.projectDate}>projectDate</div>
-            <div className={styles.taskChecklist}>taskChecklist</div>
+          <div className={styles.updateAndDeleteBtns}>
+            <button
+              type="button"
+              name="update" /* onClick={handleUpdateTask} */
+            >
+              Update Task
+            </button>
+            <button
+              type="button"
+              name="delete" /* onClick={handleDeleteTask} */
+            >
+              Delete Task
+            </button>
+          </div>
+          <div className={styles.backToProjectView}>
+            <a
+              href="#"
+              className={styles.spanContainer}
+              onClick={() => {
+                closeModal()
+              }}
+            >
+              <span className="material-icons">keyboard_arrow_left</span>
+              <span>Back to project</span>
+            </a>
           </div>
         </div>
       </div>
