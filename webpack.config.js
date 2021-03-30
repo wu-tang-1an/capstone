155df3e7c44d2d035ae48a0487c3084629012705
gene <softhaxorz@gmail.com>
2021-03-30 13:37:34 -0700

    fix: change loaders for vanilla css & css modules

--- webpack.config.js
+++ webpack.config.js
@@ -26,16 +26,23 @@ module.exports = {
         loader: 'babel-loader',
       },
       {
-        test: /\.css/,
+        test: /\.css$/,
         use: [
           'style-loader',
           {
             loader: 'css-loader',
             options: {
+              importLoaders: 1,
               modules: true,
             },
           },
         ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
       },
     ],
   },
