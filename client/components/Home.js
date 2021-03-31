685cb381aa38f01aa5a14945a48b515028c6bc70
TwelveEyes <softhaxorz@gmail.com>
2021-03-31 07:52:00 -0700

    Revert "Homepage"

--- client/components/Home.js
+++ client/components/Home.js
@@ -5,10 +5,27 @@ import styles from './css/Home.module.css'
 const Home = () => {
   const {user} = useContext(AuthContext)
 
+  console.log('user is: ', user)
+
+  if (
+    user.firstName === 'Add First Name' &&
+    user.lastName === 'Add Last Name'
+  ) {
+    return (
+      <div className={styles.homeContainer}>
+        <div className={styles.welcome}>
+          <span>Welcome!</span>
+        </div>
+        <div className={styles.myFeed}></div>
+        {/* chat component here */}
+      </div>
+    )
+  }
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.welcome}>
-        <span>Welcome, {`${user.firstName}!` || 'friend!'}</span>
+        <span>Welcome, {user.firstName}!</span>
       </div>
       <div className={styles.myFeed}></div>
       {/* chat component here */}
