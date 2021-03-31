45bb94479f5d429674175d4845fcd0533a94addb
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-31 07:57:26 -0500

    fix: back btn cursor and action

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -16,70 +16,72 @@ const Signup = () => {
 
   return (
     <section>
-      <div className={styles.signupContainer}>
+      <div className={styles.leftPanel}>
         <div className={styles.title}>
           Signup with{' '}
           <span className={styles.notearyColor}>note&#8209;ary</span>
         </div>
-        {!isFormVisible && (
-          <div className={styles.btnContainer}>
-            <button type="button" className={styles.googleSignupBtn}>
-              <span>
-                <img src="/google-logo.webp" />
+        <div className={styles.signupContainer}>
+          {!isFormVisible && (
+            <div className={styles.btnContainer}>
+              <button type="button" className={styles.googleSignupBtn}>
+                <span>
+                  <img src="/google-logo.webp" />
+                </span>
+                Signup with Google
+              </button>
+              <button
+                type="submit"
+                className={styles.localSignupBtn}
+                onClick={() => setFormVisible(!isFormVisible)}
+              >
+                Signup Direct
+              </button>
+            </div>
+          )}
+          {isFormVisible && (
+            <form>
+              <span onClick={() => setFormVisible(!isFormVisible)}>
+                Back to signup options
               </span>
-              Signup with Google
-            </button>
-            <button
-              type="submit"
-              className={styles.localSignupBtn}
-              onClick={() => setFormVisible(!isFormVisible)}
-            >
-              Signup Direct
-            </button>
-          </div>
-        )}
-        {isFormVisible && (
-          <form>
-            <span onClick={() => setFormVisible(!isFormVisible)}>
-              Back to signup options
-            </span>
-            <div>
-              <label htmlFor="firstName">First name</label>
-              <input
-                type="text"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="lastName">Last name</label>
-              <input
-                type="text"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input
-                type="text"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button type="submit" onClick={() => {}}>
-              Sign me up!
-            </button>
-          </form>
-        )}
+              <div>
+                <label htmlFor="firstName">First name</label>
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="lastName">Last name</label>
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <input
+                  type="text"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button type="submit" onClick={() => {}}>
+                Sign me up!
+              </button>
+            </form>
+          )}
+        </div>
       </div>
       <div className={styles.photo}>
         <div className={styles.transparencyMask}></div>
