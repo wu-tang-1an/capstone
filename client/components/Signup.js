91e0697d11f6444ffba544f129ae732fcf5e3a29
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-31 06:24:30 -0500

    add: signup page with form

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from './css/Signup.module.css'
 
 const Signup = () => {
+  // retrieve the user's email from landing page
   const newUserEmail = localStorage.getItem('newUserEmail')
 
-  return <div>welcome, {newUserEmail}!</div>
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState(newUserEmail || '')
+  const [password, setPassword] = useState('')
+
+  return (
+    <section>
+      <div className={styles.signupContainer}>
+        <div className={styles.title}>
+          Signup with <br />
+          <span className={styles.notearyColor}>note-ary</span>
+        </div>
+        <form>
+          <div>
+            <label htmlFor="firstName">First name</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className={styles.btnContainer}>
+            <button type="submit" className={styles.localSignup}>
+              Signup with note-ary
+            </button>
+            <button type="button" className={styles.googleSignup}>
+              <span>
+                <img src="/google-logo.webp" />
+              </span>
+              Signup with Google
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className={styles.photo}>
+        <div className={styles.transparencyMask}></div>
+      </div>
+    </section>
+  )
 }
 
 export default Signup
