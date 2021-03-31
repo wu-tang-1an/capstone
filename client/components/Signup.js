d0aecdc31b759d73831d00c9d693b9f265b1a518
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-31 07:54:41 -0500

    chore: update signup flow

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -5,65 +5,81 @@ const Signup = () => {
   // retrieve the user's email from landing page
   const newUserEmail = localStorage.getItem('newUserEmail')
 
+  // form state
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState(newUserEmail || '')
   const [password, setPassword] = useState('')
 
+  // reveal form
+  const [isFormVisible, setFormVisible] = useState(false)
+
   return (
     <section>
       <div className={styles.signupContainer}>
         <div className={styles.title}>
-          Signup with <span className={styles.notearyColor}>note-ary</span>
-          <br />
-          <span className={styles.smolTitle}>by choosing an option below.</span>
+          Signup with{' '}
+          <span className={styles.notearyColor}>note&#8209;ary</span>
         </div>
-        <div className={styles.googleBtnContainer}>
-          <button type="button" className={styles.googleBtn}>
-            <span>
-              <img src="/google-logo.webp" />
+        {!isFormVisible && (
+          <div className={styles.btnContainer}>
+            <button type="button" className={styles.googleSignupBtn}>
+              <span>
+                <img src="/google-logo.webp" />
+              </span>
+              Signup with Google
+            </button>
+            <button
+              type="submit"
+              className={styles.localSignupBtn}
+              onClick={() => setFormVisible(!isFormVisible)}
+            >
+              Signup Direct
+            </button>
+          </div>
+        )}
+        {isFormVisible && (
+          <form>
+            <span onClick={() => setFormVisible(!isFormVisible)}>
+              Back to signup options
             </span>
-            Signup with Google
-          </button>
-          <span className={styles.bigOr}> , or:</span>
-        </div>
-        <form>
-          <div>
-            <label htmlFor="firstName">First name</label>
-            <input
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last name</label>
-            <input
-              type="text"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="text"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className={styles.localSignup}>
-            Signup direct
-          </button>
-        </form>
+            <div>
+              <label htmlFor="firstName">First name</label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last name</label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" onClick={() => {}}>
+              Sign me up!
+            </button>
+          </form>
+        )}
       </div>
       <div className={styles.photo}>
         <div className={styles.transparencyMask}></div>
