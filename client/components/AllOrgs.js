5f05b4dfbd26116f879cb9896f048bbca477ad24
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-31 16:20:08 -0500

    fix: bugs related to persisting newly-deleted org and admin role

--- client/components/AllOrgs.js
+++ client/components/AllOrgs.js
@@ -6,31 +6,34 @@ import {CgOrganisation} from 'react-icons/cg'
 import {IconContext} from 'react-icons'
 import AddOrgDropdown from './AddOrgDropdown'
 import styles from './css/AllOrgs.module.css'
-import {deleteOrganizationDB, deleteUserToOrgDB} from '../context/axiosService'
-import {OrganizationContext} from '../context/organizationContext'
+import {
+  deleteOrganizationDB,
+  fetchAllOrganizations,
+} from '../context/axiosService'
 
 const AllOrgs = () => {
   // grab user from auth context
   const {user} = useContext(AuthContext)
 
-  console.log('this is user--->', user)
-
-  //grab organization from organizationContext
-  // const {organization} = useContext(OrganizationContext)
-  // console.log('organization--->', organization)
+  console.log(user)
 
   // initialize all orgs state
   const [organizations, setOrganizations] = useState([])
 
-  const fetchAllOrgs = async () => {
-    try {
-      const {data} = await axios.get(`/api/users/${user.id}/organizations`)
-      setOrganizations(data)
-    } catch (err) {
-      console.error(err)
+  // fetch all orgs
+  useEffect(() => {
+    const fetchAllOrgs = async () => {
+      try {
+        const orgs = await fetchAllOrganizations()
+        setOrganizations(orgs)
+      } catch (err) {
+        console.error(err)
+      }
     }
-  }
+    fetchAllOrgs()
+  }, [])
 
+  // delete a single org and persist to local state
   const deleteOrganization = async (e, org) => {
     e.preventDefault()
     try {
@@ -40,78 +43,6 @@ const AllOrgs = () => {
     } catch (err) {
       console.error(err)
     }
-  }
-
-  useEffect(() => {
-    let isMounted = true
-
-    fetchAllOrgs()
-
-    return () => {
-      isMounted = false
-    }
-  }, [])
-
-  if (user.status === 'admin') {
-    return (
-      <div>
-        {organizations && (
-          <div>
-            <div className={styles.headerCont}>
-              <h1 className={styles.allOrgsHeader}>Your Organizations</h1>
-
-              <IconContext.Provider
-                value={{size: '2rem', style: {marginTop: '0.7rem'}}}
-              >
-                <CgOrganisation />
-              </IconContext.Provider>
-            </div>
-
-            <div className={styles.allOrgsCont}>
-              {organizations.map((org) => (
-                <Link
-                  key={org.id}
-                  className={styles.allOrgsAnchor}
-                  to={`/organizations/${org.id}`}
-                >
-                  <div className={styles.orgCont}>
-                    <div>
-                      <img className={styles.orgImg} src={org.imageUrl} />
-                    </div>
-                    <div className={styles.orgNameCont}>
-                      <h3 className={styles.orgName}>{org.name}</h3>
-                    </div>
-                    <div onSubmit={(event) => event.preventDefault()}>
-                      <button
-                        type="button"
-                        onClick={(event) => {
-                          deleteOrganization(event, org)
-                        }}
-                      >
-                        X
-                      </button>
-                    </div>
-
-                    {/* <div onSubmit={(event) => event.preventDefault()}>
-                      <button
-                        type="button"
-                        onClick={() => {
-                          // this.props.removeSingleStudent(student)
-                          // fetchAllOrgs()
-                        }}
-                      >
-                        Edit
-                      </button>
-                    </div> */}
-                  </div>
-                </Link>
-              ))}
-              <AddOrgDropdown />
-            </div>
-          </div>
-        )}
-      </div>
-    )
   }
 
   return (
@@ -142,9 +73,22 @@ const AllOrgs = () => {
                   <div className={styles.orgNameCont}>
                     <h3 className={styles.orgName}>{org.name}</h3>
                   </div>
+                  {user && user.status === 'admin' && (
+                    <div>
+                      <button
+                        type="button"
+                        onClick={(event) => {
+                          deleteOrganization(event, org)
+                        }}
+                      >
+                        X
+                      </button>
+                    </div>
+                  )}
                 </div>
               </Link>
             ))}
+            <AddOrgDropdown />
           </div>
         </div>
       )}
