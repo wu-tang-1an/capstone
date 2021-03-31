c2edc18e6daca56757505b281f0515f633ed0fa0
gene <softhaxorz@gmail.com>
2021-03-31 14:51:50 -0700

    feat: add create project logic to add project dialog

--- client/components/AddProjectDialog.js
+++ client/components/AddProjectDialog.js
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {createProjectDb, getOrgDb} from '../context/axiosService'
+import {notify} from './helper/toast'
 
 import styles from './css/AddDialogShared.module.css'
 
-const AddProjectDialog = ({closeModal}) => {
+const validate = (name) => {
+  let errors = []
+
+  if (!name.length) errors.push('Name must not be empty!')
+
+  return errors
+}
+
+const AddProjectDialog = ({organization, setProjects, closeModal}) => {
+  const [name, setName] = useState('New Project')
+
+  const createProject = async () => {
+    const errors = validate(name)
+
+    if (errors.length) {
+      errors.forEach((error) => {
+        notify(error, 'error')
+      })
+
+      return
+    }
+
+    await createProjectDb(organization.id, {
+      name: name,
+    })
+
+    // get new organization info from db
+    const data = await getOrgDb(organization.id)
+
+    // update projects state
+    setProjects(data.projects || [])
+
+    closeModal()
+  }
+
   return (
     <div className={styles.addDropDownContainer}>
-      <textarea
-        className={styles.description}
-        // onChange={(e) => setName(e.target.value)}
-      ></textarea>
+      <div>Project name</div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
       <div className={styles.btnContainer}>
-        <button
-          type="button"
-          className={styles.addBtn}
-          //   onClick={() => {}}
-        >
+        <button type="button" className={styles.addBtn} onClick={createProject}>
           Add
         </button>
         <button type="button" className={styles.cancelBtn} onClick={closeModal}>
