1008625a335c038721ac7959761db49d59fdd234
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-31 10:46:33 -0500

    fix: convert tag-level css to class names to fix module bug

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -11,45 +11,46 @@ const LandingPage = () => {
 
   return (
     <div className={styles.homeContainer}>
-      <div className={styles.heroBg}>
-        <section>
-          <div>
-            <div className={styles.bigText}>
-              Grow your team with{' '}
-              <span className={styles.notearyColor}>note&#8209;ary</span>.
-            </div>
-            <p className={styles.mainCopy}>
-              Note-ary is a collaborative project management suite that empowers
-              colleagues to reach new heights of productivity.
-            </p>
-            {user && user.id && (
-              <div className={styles.gotoProjects}>
-                <Link to="/organizations">
-                  <button type="button">Go to my projects</button>
-                </Link>
-              </div>
-            )}
-            {user && !user.id && (
-              <form
-                className={styles.landingPageForm}
-                onSubmit={() => {
-                  localStorage.setItem('newUserEmail', email)
-                  history.push('/signup')
-                }}
-              >
-                <input
-                  type="signup"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Email"
-                />
-                <button type="submit">Sign up!</button>
-              </form>
-            )}
+      <section className={styles.leftPanel}>
+        <div className={styles.bigText}>
+          Grow your team with{' '}
+          <span className={styles.notearyColor}>note&#8209;ary</span>.
+        </div>
+        <p className={styles.mainCopy}>
+          Note-ary is a collaborative project management suite that empowers
+          colleagues to reach new heights of productivity.
+        </p>
+        {user && user.id && (
+          <div className={styles.gotoProjects}>
+            <Link to="/organizations">
+              <button type="button">Go to my projects</button>
+            </Link>
           </div>
-          <img src="https://i.imgur.com/aFiuSX9.jpg" />
-        </section>
-      </div>
+        )}
+        {user && !user.id && (
+          <form
+            className={styles.landingPageForm}
+            onSubmit={() => {
+              localStorage.setItem('newUserEmail', email)
+              history.push('/signup')
+            }}
+          >
+            <input
+              type="signup"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+            <button type="submit">Sign up!</button>
+          </form>
+        )}
+      </section>
+      <section className={styles.rightPanel}>
+        <img
+          src="https://i.imgur.com/aFiuSX9.jpg"
+          className={styles.landingPageImg}
+        />
+      </section>
     </div>
   )
 }
--- client/components/Signup.js
+++ client/components/Signup.js
@@ -49,82 +49,81 @@ const Signup = () => {
   }
 
   return (
-    <section>
+    <div className={styles.signupContainer}>
       <div className={styles.leftPanel}>
         <div className={styles.title}>
           Signup with{' '}
           <span className={styles.notearyColor}>note&#8209;ary</span>
         </div>
-        <div className={styles.signupContainer}>
-          {!isFormVisible && (
-            <div className={styles.btnContainer}>
-              <a href="/auth/google" className={styles.googleSignupBtn}>
-                <span>
-                  <img src="/google-logo.webp" />
-                </span>
-                Signup with Google
-              </a>
-              <button
-                type="submit"
-                className={styles.localSignupBtn}
-                onClick={() => setFormVisible(!isFormVisible)}
-              >
-                Signup Direct
-              </button>
-            </div>
-          )}
-          {isFormVisible && (
-            <form>
-              <span onClick={() => setFormVisible(!isFormVisible)}>
-                Back to signup options
+
+        {!isFormVisible && (
+          <div className={styles.btnContainer}>
+            <a href="/auth/google" className={styles.googleSignupBtn}>
+              <span>
+                <img src="/google-logo.webp" />
               </span>
-              <div>
-                <label htmlFor="firstName">First name</label>
-                <input
-                  type="text"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="lastName">Last name</label>
-                <input
-                  type="text"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <button
-                type="submit"
-                className={styles.signMeUp}
-                onClick={authenticateUser}
-              >
-                Sign me up!
-              </button>
-            </form>
-          )}
-        </div>
+              Signup with Google
+            </a>
+            <button
+              type="submit"
+              className={styles.localSignupBtn}
+              onClick={() => setFormVisible(!isFormVisible)}
+            >
+              Signup Direct
+            </button>
+          </div>
+        )}
+        {isFormVisible && (
+          <form className={styles.signupForm}>
+            <span onClick={() => setFormVisible(!isFormVisible)}>
+              Back to signup options
+            </span>
+            <div>
+              <label htmlFor="firstName">First name</label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last name</label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className={styles.signMeUp}
+              onClick={authenticateUser}
+            >
+              Sign me up!
+            </button>
+          </form>
+        )}
       </div>
-      <div className={styles.photo}>
+      <div className={styles.signupPhoto}>
         <div className={styles.transparencyMask}></div>
       </div>
-    </section>
+    </div>
   )
 }
 
