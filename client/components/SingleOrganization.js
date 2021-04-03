415e54d0c9a004a5b498255011b765a22bf21963
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-03 15:08:23 -0500

    refactor: single org view

--- client/components/SingleOrganization.js
+++ client/components/SingleOrganization.js
@@ -1,42 +1,44 @@
 import React, {useState, useEffect, useMemo, useContext} from 'react'
-import styled from 'styled-components'
+import {Link} from 'react-router-dom'
 import {AuthContext} from '../context/authContext'
 
 import axios from 'axios'
 import isAdmin from './helper/isAdmin'
-import AddMemberDropdown from './sub-components/AddMemberDropdown'
 
-import ProjectCard from './ProjectCard'
-import UserCard from './UserCard'
+import AddMemberDialog from './sub-components/AddMemberDialog'
+import Modal from './Modal'
+
 import styles from './css/SingleOrganization.module.css'
-import AddProjectDropdown from './AddProjectDropdown'
 import {errorRedirect} from './helper/errorHandle'
 
-const OverflowWrapper = styled.div`
-  height: 400px;
-  overflow: auto;
-`
-
-const ProjectFrame = () => {
+const ProjectFrame = ({project}) => {
+  const {id, name, imageUrl, description, status} = project
   return (
-    <div className={styles.projectCardContainer}>
-      <Link to={`/projects/${projectId}`}>
+    <Link to={`/projects/${id}`}>
+      <div className={styles.projectCardContainer}>
         <div className={styles.cardContents}>
           <img src={imageUrl} />
-          <div className={styles.orgName}>{name}</div>
-          <div className={styles.numMembers}>{`${numMembers} members`}</div>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.status}>{status}</div>
+          <div className={styles.description}>{description}</div>
         </div>
-      </Link>
-      <button
-        className={styles.openModalBtn}
-        type="button"
-        onClick={() => {
-          setOrgIdForDelete(orgId)
-          setModalVisible(true)
-        }}
-      >
-        Leave Organization
-      </button>
+      </div>
+    </Link>
+  )
+}
+
+const Member = ({member, removeUser}) => {
+  const {name, imageUrl, role} = member
+  return (
+    <div className={styles.memberContainer}>
+      <div className={styles.imgAndInfo}>
+        <img className={styles.avatar} src={imageUrl} />
+        <div className={styles.nameAndRole}>
+          <div className={styles.role}>{role}</div>
+          <div className={styles.name}>{name}</div>
+        </div>
+      </div>
+      <button type="button" onClick={removeUser}></button>
     </div>
   )
 }
@@ -105,6 +107,9 @@ const SingleOrganization = ({match}) => {
     }
   }, [organization, projects, status])
 
+  // handle modal visibility
+  const [isModalVisible, setModalVisible] = useState(false)
+
   // destructure organization
   const {name, imageUrl} = organization
 
@@ -113,54 +118,30 @@ const SingleOrganization = ({match}) => {
   }
 
   return (
-    <div className={styles.organizationContainer}>
-      <div className={styles.header}>Organization: {organization.name}</div>
-      <img className={styles.organizationImg} src={imageUrl} />
-      <div className={styles.membersAndProjects}>
-        <div className={styles.membersContainer}>
-          <div className={styles.membersTitle}>Members:</div>
-          <OverflowWrapper>
-            {status ? <AddMemberDropdown orgId={organizationId} /> : null}
-
-            <div className={styles.memberList}>
-              {members &&
-                members.map((member) => (
-                  <UserCard
-                    key={member.id}
-                    user={{
-                      ...member,
-                      orgId: organizationId,
-                      removeUser: removeUser,
-                      mainUser: user,
-                    }}
-                  />
-                ))}
-            </div>
-          </OverflowWrapper>
+    <div className={styles.wrapper}>
+      {isModalVisible && (
+        <Modal>
+          <AddMemberModal closeModal={() => setModalVisible(false)} />
+        </Modal>
+      )}
+      <section className={styles.leftPanel}>
+        {members.map((member) => (
+          <Member key={member.id} member={member} removeUser={removeUser} />
+        ))}
+        <AddMemberDialog members={members} setMembers={setMembers} />
+      </section>
+      <section className={styles.rightPanel}>
+        <div className={styles.avatarAndName}>
+          <img className={styles.organizationImg} src={imageUrl} />
+          <div className={styles.sectionHeader}>Organization: {name}</div>
         </div>
-
-        <div className={styles.projectsContainer}>
-          <div className={styles.projectsTitle}>Projects:</div>
-          <div className={styles.projectList}>
-            <OverflowWrapper>
-              <AddProjectDropdown
-                organization={organization}
-                setProjects={setProjects}
-              />
-              {projects &&
-                projects.map((project) => (
-                  <div key={project.id}>
-                    <ProjectCard
-                      project={project}
-                      organization={organization}
-                      setProjects={setProjects}
-                    />
-                  </div>
-                ))}
-            </OverflowWrapper>
-          </div>
+        <div className={styles.subsectionHeader}>Projects</div>
+        <div className={styles.allProjectsContainer}>
+          {projects.map((project) => (
+            <ProjectFrame key={project.id} project={project} />
+          ))}
         </div>
-      </div>
+      </section>
     </div>
   )
 }
