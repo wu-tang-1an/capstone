479392124f0c00d57225e063794290a44b1238c7
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-03 13:52:06 -0500

    add: create org modal styling

--- client/components/AddOrganizationDialog.js
+++ client/components/AddOrganizationDialog.js
@@ -14,58 +14,68 @@ const NewOrganizationForm = ({
   const [imageUrl, setImageUrl] = useState('')
 
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault()
-        try {
-          const createdOrg = await addOrganizationDB({
-            userId,
-            newOrg: {
-              name,
-              imageUrl,
-            },
-          })
+    <div className={styles.modalContent}>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault()
+          try {
+            const createdOrg = await addOrganizationDB({
+              userId,
+              newOrg: {
+                name,
+                imageUrl,
+              },
+            })
 
-          console.log(createdOrg)
+            console.log(createdOrg)
 
-          setOrganizations([...organizations, createdOrg])
-          closeModal()
-        } catch (err) {
-          console.error(err)
-        }
-      }}
-    >
-      <div className={styles.formField}>
-        <label htmlFor="Organization name" />
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
+            setOrganizations([...organizations, createdOrg])
+            closeModal()
+          } catch (err) {
+            console.error(err)
+          }
+        }}
+      >
+        <div className={styles.formField}>
+          <label htmlFor="Organization name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-      <div className={styles.formField}>
-        <pre>
-          <img src={imageUrl} />
-        </pre>
-      </div>
+        <div className={styles.formField}>
+          <pre>
+            <img src={imageUrl} />
+          </pre>
+        </div>
 
-      <div className={styles.formField}>
-        <label htmlFor="Organization image" />
-        <input
-          type="text"
-          name="imageUrl"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
-      </div>
+        <div className={styles.formField}>
+          <label htmlFor="Organization image">Image url</label>
+          <input
+            type="text"
+            name="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+        </div>
 
-      <button type="submit">Create My Organization</button>
-      <button type="button" onClick={closeModal}>
-        Cancel
-      </button>
-    </form>
+        <div className={styles.modalBtnsContainer}>
+          <button className={styles.createBtn} type="submit">
+            Create Organization
+          </button>
+          <button
+            className={styles.cancelBtn}
+            type="button"
+            onClick={closeModal}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
